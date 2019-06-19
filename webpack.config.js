74f132e065dd0d5e8fee27cb4b0f4ad76a5dd070
gidraff <kamandegid@gmail.com>
2019-06-19 22:57:05 +0300

    feat(webpack): setup webpack config - add npm packages - add babelrc file - add linting with eslint - edit readme file [Delivers Webpac setup]

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const SRC_DIR = `${__dirname }/src`;
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    hot: true,
+    compress: true,
+    watchContentBase: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js?)$/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        options: {
+          emitWarning: true,
+          configFile: './.eslintrc.js',
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(html)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'html-loader',
+          options: { minimize: true },
+        },
+      },
+      {
+        test: /(\.css|\.scss|\.sass)$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'sass-loader',
+        }],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${SRC_DIR }/index.html`,
+      filename: './index.html',
+    }),
+  ],
+};
